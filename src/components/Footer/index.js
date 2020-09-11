391e7ede166c829852dcf820588fdc9c1d20cc18
Seth Purr <eric.purrington@gmail.com>
2020-09-11 15:03:27 -0600

    added my mountains as the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./style.css";
+import MyMounts from "../../images/MyMounts.jpg";
 
 function Footer() {
     return (
-        <div className="uk-container uk-container-expand footer">
-            <div className="textHolder uk-text-center">
-                <a href="https://github.com/eric-purrington" target="_blank" rel="noopener noreferrer"><i className="fab fa-github fa-2x"></i></a>
-                <a href="https://www.linkedin.com/in/ericpurrington/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in fa-2x"></i></a>
-                <a href="mailto:eric.purrington@gmail.com" target="_blank" rel="noopener noreferrer"><i className="fas fa-envelope fa-2x"></i></a>
-                <p>&copy; Eric Purrington 2020</p>
+        <div>
+            <img className="mountains" src={MyMounts} alt="mybluemountains" />
+            <div className="uk-container uk-container-expand footer">
+                <div className="textHolder uk-text-center">
+                    <a href="https://github.com/eric-purrington" target="_blank" rel="noopener noreferrer"><i className="fab fa-github fa-2x"></i></a>
+                    <a href="https://www.linkedin.com/in/ericpurrington/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in fa-2x"></i></a>
+                    <a href="mailto:eric.purrington@gmail.com" target="_blank" rel="noopener noreferrer"><i className="fas fa-envelope fa-2x"></i></a>
+                    <p>&copy; Eric Purrington 2020</p>
+                </div>
             </div>
         </div>
     )
